4d381fb59bde88d4996a0ec5e070a8ba022a9893
boris <boris2code@outlook.com>
2024-03-29 16:58:44 +0800

    feat: share collaborators get

--- apps/nestjs-backend/src/features/record/record.service.ts
+++ apps/nestjs-backend/src/features/record/record.service.ts
@@ -812,7 +812,6 @@ export class RecordService implements IAdapterService {
 
     const fields = await this.getFieldsByProjection(tableId, projectionInner, fieldKeyType);
     const fieldNames = fields.map((f) => f.dbFieldName).concat(Array.from(preservedDbFieldNames));
-
     const nativeQuery = this.knex(dbTableName)
       .select(fieldNames)
       .whereIn('__id', recordIds)
@@ -843,7 +842,6 @@ export class RecordService implements IAdapterService {
     });
 
     const primaryField = createFieldInstanceByRaw(primaryFieldRaw);
-
     const snapshots = result
       .sort((a, b) => {
         return recordIdsMap[a.__id] - recordIdsMap[b.__id];
